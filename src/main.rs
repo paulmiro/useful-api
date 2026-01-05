9691e404ddd6efbfdc77396996a6764f7ab91ae6
paulmiro <30203227+paulmiro@users.noreply.github.com>
2026-01-05 20:17:06 +0100

    dedup calculation

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use axum::{Router, routing::get};
 use axum::debug_handler;
-use serde::Deserialize;
-use tokio::sync::Mutex;
-use std::time::{Duration, Instant};
+use axum::{Router, routing::get};
 use lazy_static::lazy_static;
+use serde::Deserialize;
+use std::time::{Duration, Instant};
+use tokio::sync::Mutex;
 
 lazy_static! {
-    static ref CACHE: Mutex<(Option<f64>, Option<Instant>)> = Mutex::new((None, None));
+    static ref CACHE: Mutex<Option<SatoshiPriceCache>> = Mutex::new(None);
+}
+
+#[derive(Clone, Copy)]
+struct SatoshiPriceCache {
+    price: f64,
+    time: Instant,
 }
 
 #[derive(Deserialize)]
@@ -44,32 +50,40 @@ async fn hello_handler() -> String {
 #[debug_handler]
 async fn mensatoshi_handler() -> String {
     let mut cache = CACHE.lock().await;
-    let (cached_price, last_fetch) = *cache;
 
-    if let (Some(price), Some(fetch_time)) = (cached_price, last_fetch) {
-        if fetch_time.elapsed() < Duration::from_secs(300) {
-            let satoshi_per_btc = 100_000_000.0;
-            let satoshi_per_eur = satoshi_per_btc / price;
-            let mensa_price_eur = 1.20;
-            let mensasatoshi = mensa_price_eur * satoshi_per_eur;
-            return mensasatoshi.round().to_string();
+    let satoshi_per_eur = match *cache {
+        Some(price_cache) if price_cache.time.elapsed() < Duration::from_secs(300) => {
+            price_cache.price
         }
-    }
-
-    let url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur";
-    match reqwest::get(url).await {
-        Ok(response) => match response.json::<CoinGeckoResponse>().await {
-            Ok(data) => {
-                let eur_per_btc = data.bitcoin.eur;
-                *cache = (Some(eur_per_btc), Some(Instant::now()));
-                let satoshi_per_btc = 100_000_000.0;
-                let satoshi_per_eur = satoshi_per_btc / eur_per_btc;
-                let mensa_price_eur = 1.20;
-                let mensasatoshi = mensa_price_eur * satoshi_per_eur;
-                mensasatoshi.round().to_string()
+        _ => {
+            let url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur";
+            let response = match reqwest::get(url).await {
+                Ok(response) => response,
+                Err(_) => return "Error fetching data from CoinGecko".to_string(),
+            };
+            match response.json::<CoinGeckoResponse>().await {
+                Ok(data) => {
+                    let eur_per_btc = data.bitcoin.eur;
+                    let satoshi_per_btc: f64 = 100_000_000.0;
+                    let satoshi_per_eur = satoshi_per_btc / eur_per_btc;
+                    *cache = Some(SatoshiPriceCache {
+                        price: satoshi_per_eur,
+                        time: Instant::now(),
+                    });
+                    satoshi_per_eur
+                }
+                Err(_) => {
+                    return "Error deserializing CoinGecko response. Probably rate limited."
+                        .to_string();
+                }
             }
-            Err(_) => "Error deserializing response".to_string(),
-        },
-        Err(_) => "Error fetching data from CoinGecko".to_string(),
-    }
+        }
+    };
+
+    let mensa_price_eur = 1.20; // TOOO: fetch dynamically
+    let mensasatoshi = mensa_price_eur * satoshi_per_eur;
+    return format!(
+        "Der Mensa-Eintopf kostet aktuell {} Satoshi.",
+        mensasatoshi.round()
+    );
 }
